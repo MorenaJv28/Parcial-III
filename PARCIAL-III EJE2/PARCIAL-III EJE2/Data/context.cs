b86590713fc552e2b33855f6c13800ea7aab7a35
MorenaJv28 <124007217+MorenaJv28@users.noreply.github.com>
2023-11-18 15:50:06 -0600

    EJE2

---
+++ PARCIAL-III EJE2/PARCIAL-III EJE2/Data/context.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PARCIAL_III_EJE2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PARCIAL_III_EJE2.Data
+{
+    public class context : DbContext
+    {
+        public DbSet<notas> notas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=BERA_JOVEL\\BERAJOVEL;Database=PROGRA;Trusted_Connection=SSPI;MultipleActiveResultSets=true;TrustServerCertificate=true;");
+        }
+    }
+}
---
+++ PARCIAL-III EJE2/PARCIAL-III EJE2/Models/models.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PARCIAL_III_EJE2.Models
+{
+    public class notas
+    {
+        public int id {  get; set; }
+        public string? estudiante { get; set; }
+        public double parciales { get; set; }
+        public double laboratorio { get; set; }
+        public double final { get; set; }
+
+    }
+}
